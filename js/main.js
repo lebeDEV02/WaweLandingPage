dbe0ee40819160103c9843ebef2a68056dd63c9c
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-08 13:01:35 +0500

    added smooth scroll for menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 $(function () {
+	$(".menu__list").on("click", "a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
 	$('.blog__slider').slick({
 		arrows: false,
 		dots: true
 	})
-	$('.burger__menu').on('click', function () {
+	$('.burger__menu, .item__link').on('click', function () {
 		$('.menu__list').toggleClass('menu__list--active');
 	});
 	var mixer = mixitup('.gallery__images');
